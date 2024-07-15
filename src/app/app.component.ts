b9a6631c9e8c571aabeaf47111402ee40d559917
NehaPoddarB <neha.kumari@nashtechglobal.com>
2024-07-15 11:03:58 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'excel-file-reader';
+  
+  data = [['index1', 'index2'], ['index3', 'index4']];
+  opts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
+  fileName: string = 'SheetJS.xlsx';
+
+  onFileChange(evt: any) {
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+
+      /* To read workbook */
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+      /* grab first sheet to read*/
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data and add logic */
+      this.data = <any>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
+      console.log(this.data);
+    };
+    reader.readAsBinaryString(target.files[0]);
+  }
+
+
+  export(): void {
+    /* generate worksheet */
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, this.fileName);
+  }
+
 }
